4f30c0f76671f4a8b7efe68068488adb223ca433
RuslanMck <mckruslan@gmail.com>
2018-09-21 10:15:42 +0300

    2018.09.21

---
+++ ArrayMerging/src/ArrayMerging.java
@@ -0,0 +1,33 @@
+public class ArrayMerging {
+
+    public static void main(String[] args) {
+
+        int[] arrFst = {0, 1, 5, 7, 8};
+        int[] arrSec = {2, 3, 4, 6, 9};
+        int[] arrRes = new int[arrFst.length + arrSec.length];
+        int varArrFst = 0;
+        int varArrSec = 0;
+
+        for (int i = 0; i < arrRes.length; i++) {
+            if (varArrFst < arrFst.length && varArrSec < arrRes.length) {
+                if (arrSec[varArrSec] < arrFst[varArrFst]) {
+                    arrRes[i] = arrSec[varArrSec];
+                    varArrSec++;
+                } else {
+                    arrRes[i] = arrFst[varArrFst];
+                    varArrFst++;
+                }
+            } else if (varArrSec < arrSec.length) {
+                arrRes[i] = arrSec[varArrSec];
+                varArrSec++;
+            } else {
+                arrRes[i] = arrFst[varArrFst];
+                varArrFst++;
+            }
+        }
+
+        for (int i = 0; i < arrRes.length; i++) {
+            System.out.print(arrRes[i] + " ");
+        }
+    }
+}
--- EasternExpress/src/Interface.java
+++ EasternExpress/src/Interface.java
@@ -8,23 +8,18 @@ public class Interface extends JPanel {
     int IdxSec;
     int dist;
     private JTextField output = new JTextField("Distance");
+    String[] stationsList = {"Paris", "Lausanne", "Milan", "Venice", "Belgrad", "Sofia", "Istanbul"};
+    int[] distance = {498, 337, 269, 789, 394, 550};
+
 
     public Interface() {
         setLayout(null);
 
-        JLabel welcome = new JLabel("Select destination points");
-        welcome.setBounds(35, 10, 200, 25);
-        welcome.setHorizontalAlignment(0);
-        welcome.setOpaque(true);
-        add(welcome);
-
-        JLabel firstStationLabel = new JLabel("From: ");
-        firstStationLabel.setBounds(25, 50, 100, 25);
-        firstStationLabel.setHorizontalAlignment(0);
-        firstStationLabel.setOpaque(true);
-        add(firstStationLabel);
-
-        String[] stationsList = {"Paris", "Lausanne", "Milan", "Venice", "Belgrad", "Sofia", "Istanbul"};
+        createLabel("Welcome", 35,10,200,25,true,0);
+        createLabel("From:", 25,50,100,25,true,0);
+        createLabel("To:", 25,100,100,25,true,0);
+        createButton("Search", 25,150,100,25);
+        createOutput(150,150,100,25,true,true,0);
 
         JComboBox CB1 = new JComboBox(stationsList);
         CB1.setEditable(false);
@@ -38,12 +33,6 @@ public class Interface extends JPanel {
         });
         add(CB1);
 
-        JLabel secStationLabel = new JLabel("To: ");
-        secStationLabel.setBounds(25, 100, 100, 25);
-        secStationLabel.setOpaque(true);
-        secStationLabel.setHorizontalAlignment(0);
-        add(secStationLabel);
-
         JComboBox CB2 = new JComboBox(stationsList);
         CB2.setEditable(false);
         CB2.setBounds(150, 100, 100, 25);
@@ -55,24 +44,34 @@ public class Interface extends JPanel {
             }
         });
         add(CB2);
+    }
 
-        JButton button = new JButton("Search");
-        button.setBounds(25, 150, 100, 25);
-        add(button);
-        button.addActionListener(new buttonListenet());
-
-        output.setBounds(150, 150, 100, 25);
+    public void createOutput (int x, int y, int w, int h, boolean opaque, boolean editable, int alignment){
+        output.setBounds(x,y,w,h);
         output.setOpaque(true);
         output.setEditable(false);
         output.setHorizontalAlignment(0);
         add(output);
     }
 
+    public void createButton (String name, int x, int y, int w, int h) {
+        JButton button = new JButton(name);
+        button.setBounds(x,y,w,h);
+        add(button);
+        button.addActionListener(new buttonListenet());
+    }
+
+    public void createLabel (String name, int x, int y, int w, int h, boolean opaque, int alignment){
+        JLabel Label = new JLabel(name);
+        Label.setBounds(x, y, w, h);
+        Label.setOpaque(true);
+        Label.setHorizontalAlignment(0);
+        add(Label);
+    }
+
     public class buttonListenet implements ActionListener {
+
         public void actionPerformed(ActionEvent event) {
-
-            int[] distance = {498, 337, 269, 789, 394, 550};
-
             if (IdxFst > IdxSec) {
                 IdxFst = IdxFst + IdxSec;
                 IdxSec = IdxFst - IdxSec;
