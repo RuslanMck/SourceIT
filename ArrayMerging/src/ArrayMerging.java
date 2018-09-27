d6c41a7efe89105d2e1cef433f3ef0a55964011f
RuslanMck <mckruslan@gmail.com>
2018-09-27 11:48:33 +0300

    2018.09.27

--- ArrayMerging/src/ArrayMerging.java
+++ ArrayMerging/src/ArrayMerging.java
@@ -4,20 +4,20 @@ public class ArrayMerging {
         int[] arrFst = {0, 1, 5, 7, 8};
         int[] arrSec = {2, 3, 4, 6, 9};
         int[] arrRes = new int[arrFst.length + arrSec.length];
-        int varArrFst = 0;
-        int varArrSec = 0;
+        int varFst = 0;
+        int varSec = 0;
 
         for (int i = 0; i < arrRes.length; i++) {
-            if (varArrFst < arrFst.length && varArrSec < arrRes.length) {
-                if (arrSec[varArrSec] < arrFst[varArrFst]) {
-                    arrRes[i] = arrSec[varArrSec++];
+            if (varFst < arrFst.length && varSec < arrRes.length) {
+                if (arrSec[varSec] < arrFst[varFst]) {
+                    arrRes[i] = arrSec[varSec++];
                 } else {
-                    arrRes[i] = arrFst[varArrFst++];
+                    arrRes[i] = arrFst[varFst++];
                 }
-            } else if (varArrSec < arrSec.length) {
-                arrRes[i] = arrSec[varArrSec++];
+            } else if (varSec < arrSec.length) {
+                arrRes[i] = arrSec[varSec++];
             } else {
-                arrRes[i] = arrFst[varArrFst++];
+                arrRes[i] = arrFst[varFst++];
             }
         }
         for (int i = 0; i < arrRes.length; i++) {
--- ChoiceSorting/src/ChoiceSorting.java
+++ ChoiceSorting/src/ChoiceSorting.java
@@ -1,41 +1,32 @@
+import java.util.Arrays;
+
 public class ChoiceSorting {
 
     public static void main(String[] args) {
 
-        int arrOfNumb[] = {4, 7, 1, 9, 2, 5, 3, 6, 8};
-        System.out.println(PrintArray(arrOfNumb));
-        arraySorting(arrOfNumb);
-        System.out.println(PrintArray(arrOfNumb));
+        int arr[] = {4, 7, 1, 9, 2, 5, 3, 6, 8};
+        System.out.println(Arrays.toString(arr));
+        arraySorting(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] arraySorting (int[] arrOfNumb) {
-        for (int i = 0; i < arrOfNumb.length; i++){
-            int min = arrOfNumb[i];
+    public static int[] arraySorting (int[] arr) {
+        for (int i = 0; i < arr.length; i++){
+            int min = arr[i];
             int idx = i;
 
-            for (int j = i + 1; j < arrOfNumb.length; j++){
-                if (arrOfNumb[j] < min){
-                    min = arrOfNumb[j];
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < min){
+                    min = arr[j];
                     idx = j;
                 }
             }
             if (i != idx){
-                int tmp = arrOfNumb[i];
-                arrOfNumb[i] = arrOfNumb [idx];
-                arrOfNumb[idx] = tmp;
+                int tmp = arr[i];
+                arr[i] = arr [idx];
+                arr[idx] = tmp;
             }
         }
-        return arrOfNumb;
+        return arr;
     }
-
-    public static String PrintArray(int[] someArray) {
-
-        String print = "";
-
-        for (int i = 0; i < someArray.length; i++){
-            print += someArray[i] + " ";
-        }
-        return print;
-    }
-
 }
